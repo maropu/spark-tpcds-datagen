ec435a9801115820370afb3bf6a86cd2b8d1ac6d
Takeshi Yamamuro <yamamuro@apache.org>
2017-07-21 09:34:57 +0900

    Add script to run TPCDS benchmarks

---
+++ bin/utils.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Common functions in scripts
+
+# Function to run a given command
+run_cmd() {
+  local command="$1"
+  local working_dir="$2"
+
+  # Preserve the calling directory
+  _CALLING_DIR="$(pwd)"
+
+  # Run the given command and check if it works well
+  cd ${working_dir} && ${command}
+  if [ $? = 127 ]; then
+    echo "Cannot run '${command}', so check if the command works"
+    exit 1
+  fi
+
+  # Reset the current working directory
+  cd "${_CALLING_DIR}"
+}
+
